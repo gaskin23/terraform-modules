731297bda71c43d374c5287b620656674259194f
GOKHAN ASKIN <90231677+gaskin23@users.noreply.github.com>
2024-04-09 17:14:28 +0300

    fd

--- rds/secrets.tf
+++ rds/secrets.tf
@@ -24,7 +24,7 @@ resource "random_password" "postgresql_password" {
 
 
 resource "aws_secretsmanager_secret" "db_passwords" {
-  name        = "db-passwords2"
+  name        = "db-passwords"
 }
 
 resource "aws_secretsmanager_secret_version" "db_passwords_version" {
