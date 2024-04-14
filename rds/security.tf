31f2aed1cbd96453e77b9a9ce8d76feeb0f3fa31
GOKHAN ASKIN <90231677+gaskin23@users.noreply.github.com>
2024-04-14 22:50:35 +0300

    fd

--- rds/security.tf
+++ rds/security.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "rds_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = data.aws_security_group.eks_sg.id
+    security_groups = [data.aws_security_group.eks_sg.id]
   }
 
   egress {
